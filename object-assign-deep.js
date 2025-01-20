792115d726d1c3c4bc13cbef0a61b6c68e747fc0
Yusuke Kamiyamane <y@y14e.com>
2025-01-20 19:05:40 +0900

    20250120

--- object-assign-deep.js
+++ object-assign-deep.js
@@ -1,8 +1,9 @@
-function objectAssignDeep(target, source = {}) {
-  for (const [key, value] of Object.entries(source)) {
-    target[key] = value && typeof value === 'object' && !Array.isArray(value) ? objectAssignDeep(structuredClone(target[key] || {}), value) : structuredClone(value);
+function objectAssignDeep(target, ...sources) {
+  for (const source of sources) {
+    for (const [key, value] of Object.entries(source || {})) {
+      target[key] = value && typeof value === 'object' && !Array.isArray(value) ? objectAssignDeep(structuredClone(target[key] || {}), value) : structuredClone(value);
+    }
   }
   return target;
 }
-
 export { objectAssignDeep };
--- object-assign-deep.ts
+++ object-assign-deep.ts
@@ -1,8 +1,10 @@
-function objectAssignDeep<T extends Record<string, any>, U extends Record<string, any>>(target: T, source: U = {} as U): T & U {
-  for (const [key, value] of Object.entries(source)) {
-    (target as Record<string, any>)[key] = value && typeof value === 'object' && !Array.isArray(value) ? objectAssignDeep(structuredClone(target[key] || {}), value) : structuredClone(value);
+function objectAssignDeep<T extends Record<string, any>, U extends Record<string, any>[]>(target: T, ...sources: U): T & U[number] {
+  for (const source of sources) {
+    for (const [key, value] of Object.entries(source || {})) {
+      (target as Record<string, any>)[key] = value && typeof value === 'object' && !Array.isArray(value) ? objectAssignDeep(structuredClone(target[key] || {}), value) : structuredClone(value);
+    }
   }
-  return target as T & U;
+  return target as T & U[number];
 }
 
 export { objectAssignDeep };
