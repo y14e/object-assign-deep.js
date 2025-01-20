f6b0a774a9d795e3db7f409ecf8a0395b0ab39b6
Yusuke Kamiyamane <y@y14e.com>
2025-01-20 17:43:24 +0900

    20250120

--- object-asign-deep.js
+++ object-asign-deep.js
@@ -1,4 +1,4 @@
-function objectAssignDeep(target, source) {
+function objectAssignDeep(target, source = {}) {
   for (const [key, value] of Object.entries(source)) {
     target[key] = value && typeof value === 'object' && !Array.isArray(value) ? objectAssignDeep(structuredClone(target[key] || {}), value) : structuredClone(value);
   }
--- object-asign-deep.ts
+++ object-asign-deep.ts
@@ -1,4 +1,4 @@
-function objectAssignDeep<T extends Record<string, any>, U extends Record<string, any>>(target: T, source: U): T & U {
+function objectAssignDeep<T extends Record<string, any>, U extends Record<string, any>>(target: T, source: U = {} as U): T & U {
   for (const [key, value] of Object.entries(source)) {
     (target as Record<string, any>)[key] = value && typeof value === 'object' && !Array.isArray(value) ? objectAssignDeep(structuredClone(target[key] || {}), value) : structuredClone(value);
   }
