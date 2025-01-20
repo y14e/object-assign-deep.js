ee0e5a10d773f21095ec589fbf36967fa7b5e000
Yusuke Kamiyamane <y@y14e.com>
2025-01-20 20:43:23 +0900

    Update object-assign-deep.ts

--- object-assign-deep.ts
+++ object-assign-deep.ts
@@ -1,10 +1,10 @@
-function objectAssignDeep<T extends Record<string, any>, U extends Record<string, any>[]>(target: T, ...sources: U): T & U[number] {
+function objectAssignDeep<T extends Record<string, any>>(target: T, ...sources: Record<string, any>[]): T {
   for (const source of sources) {
     for (const [key, value] of Object.entries(source || {})) {
-      (target as Record<string, any>)[key] = value && typeof value === 'object' && !Array.isArray(value) ? objectAssignDeep(structuredClone(target[key] || {}), value) : structuredClone(value);
+      (target as Record<string, any>)[key] = value && typeof value === 'object' && !Array.isArray(value) ? objectAssignDeep(structuredClone(target[key] || {}) as Record<string, any>, value) : structuredClone(value);
     }
   }
-  return target as T & U[number];
+  return target as T;
 }
 
 export { objectAssignDeep };
