d6bea7a51461f08e45b520d37260b72e479ef9a3
Yusuke Kamiyamane <y@y14e.com>
2025-01-26 00:13:47 +0900

    Update object-assign-deep.ts

--- object-assign-deep.ts
+++ object-assign-deep.ts
@@ -1,10 +1,10 @@
-function objectAssignDeep<T extends object, U extends any[]>(target: T, ...sources: U): T & U {
+function objectAssignDeep<T extends object, U extends any[]>(target: T, ...sources: U): T {
   for (const source of sources) {
     for (const [key, value] of Object.entries(source || {})) {
       target[key] = value && typeof value === 'object' && !Array.isArray(value) ? objectAssignDeep(structuredClone(target[key] || {}), value) : structuredClone(value);
     }
   }
-  return target as T & U;
+  return target as T;
 }
 
 export { objectAssignDeep };
